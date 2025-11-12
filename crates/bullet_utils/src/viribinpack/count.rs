79cea9b5d8673189663d0eced0570a112dc23366
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-11-12 21:29:14 +0000

    Only count positions that will be used

--- crates/bullet_utils/src/viribinpack/count.rs
+++ crates/bullet_utils/src/viribinpack/count.rs
@@ -19,21 +19,45 @@ impl CountOptions {
         let mut reader = BufReader::new(file);
         let mut games = 0usize;
         let mut positions = 0usize;
+        let mut kept = 0;
+        let mut filtered = 0;
 
         let mut wins = 0;
         let mut losses = 0;
         let mut draws = 0;
 
         let mut buffer = Vec::new();
+        let mut boardsbuffer = Vec::new();
+
+        let filter = viriformat::dataformat::Filter::default();
 
         while let Ok(game) = Game::deserialise_from(&mut reader, buffer) {
             games += 1;
+
+            game.splat_to_bulletformat(|b| {
+                    boardsbuffer.push(b);
+                    Ok(())
+                },
+                &filter,
+            ).unwrap();
+
             positions += game.moves.len();
+
+            let actual_positions_after_filtering = boardsbuffer.len();
+            let filtered_in_this_game = game.moves.len() - actual_positions_after_filtering;
+            kept += actual_positions_after_filtering;
+            filtered += filtered_in_this_game;
 
             if games % 16384 == 0 {
                 print!("Counted {games} games\r");
             }
 
+            if game.moves.len() == 0 {
+                buffer = game.moves;
+                buffer.clear();
+                boardsbuffer.clear();
+                continue;
+            }
 
             match game.outcome() {
                 viriformat::dataformat::WDL::Win => wins += 1,
@@ -43,12 +67,13 @@ impl CountOptions {
 
             buffer = game.moves;
             buffer.clear();
+            boardsbuffer.clear();
         }
 
         println!();
         println!("Summary:");
         println!("Games = {games}");
-        println!("Positions = {positions}");
+        println!("Positions = {positions} (kept={kept}, filtered={filtered})");
         println!("Wins = {wins}, Draws = {draws}, Losses = {losses}");
         println!("Bytes per position = {}", bytes as f64 / positions as f64);
 
