97ce1e0f58bfa5b36cae0f90496cc4ab225a4b7c
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-12-18 16:48:36 +0000

    Add output buckets

--- crates/tcheran/src/main.rs
+++ crates/tcheran/src/main.rs
@@ -1,13 +1,12 @@
-use bullet_lib::value::loader::ViriBinpackLoader;
 use bullet_lib::{
-    game::inputs::Chess768,
+    game::{inputs::Chess768, outputs::MaterialCount},
     nn::optimiser::AdamW,
     trainer::{
         save::SavedFormat,
         schedule::{TrainingSchedule, TrainingSteps, lr, wdl},
         settings::LocalSettings,
     },
-    value::ValueTrainerBuilder,
+    value::{ValueTrainerBuilder, loader::ViriBinpackLoader},
 };
 
 const SCALE: i32 = 400;
@@ -18,29 +17,34 @@ fn main() {
     let hidden_size: usize = 1024;
     let wdl_proportion: f32 = 0.4;
     let superbatches: usize = 80;
+    const N_OUTPUT_BUCKETS: usize = 8;
 
     let mut trainer = ValueTrainerBuilder::default()
         .use_threads(8)
         .dual_perspective()
         .optimiser(AdamW)
         .inputs(Chess768)
+        .output_buckets(MaterialCount::<N_OUTPUT_BUCKETS>)
         .save_format(&[
             SavedFormat::id("l0w").round().quantise::<i16>(QA),
             SavedFormat::id("l0b").round().quantise::<i16>(QA),
-            SavedFormat::id("l1w").round().quantise::<i16>(QB),
+            SavedFormat::id("l1w")
+                .round()
+                .quantise::<i16>(QB)
+                .transpose(),
             SavedFormat::id("l1b").round().quantise::<i16>(QA * QB),
         ])
         .loss_fn(|output, target| output.sigmoid().squared_error(target))
-        .build(|builder, stm_inputs, ntm_inputs| {
+        .build(|builder, stm_inputs, ntm_inputs, output_buckets| {
             // weights
             let l0 = builder.new_affine("l0", 768, hidden_size);
-            let l1 = builder.new_affine("l1", 2 * hidden_size, 1);
+            let l1 = builder.new_affine("l1", 2 * hidden_size, N_OUTPUT_BUCKETS);
 
             // inference
             let stm_hidden = l0.forward(stm_inputs).screlu();
             let ntm_hidden = l0.forward(ntm_inputs).screlu();
             let hidden_layer = stm_hidden.concat(ntm_hidden);
-            l1.forward(hidden_layer)
+            l1.forward(hidden_layer).select(output_buckets)
         });
 
     let schedule = TrainingSchedule {
@@ -52,14 +56,30 @@ fn main() {
             start_superbatch: 1,
             end_superbatch: superbatches,
         },
-        wdl_scheduler: wdl::ConstantWDL { value: wdl_proportion },
-        lr_scheduler: lr::CosineDecayLR { initial_lr: 0.001, final_lr: 0.001 * 0.3f32.powi(5), final_superbatch: superbatches },
+        wdl_scheduler: wdl::ConstantWDL {
+            value: wdl_proportion,
+        },
+        lr_scheduler: lr::CosineDecayLR {
+            initial_lr: 0.001,
+            final_lr: 0.001 * 0.3f32.powi(5),
+            final_superbatch: superbatches,
+        },
         save_rate: 10,
     };
 
-    let settings = LocalSettings { threads: 8, test_set: None, output_directory: "checkpoints", batch_queue_size: 32 };
+    let settings = LocalSettings {
+        threads: 8,
+        test_set: None,
+        output_directory: "checkpoints",
+        batch_queue_size: 32,
+    };
 
-    let data = ViriBinpackLoader::new("data/data.viri", 1024 * 20, 4, viriformat::dataformat::Filter::default());
+    let data = ViriBinpackLoader::new(
+        "data/data.viri",
+        1024 * 20,
+        4,
+        viriformat::dataformat::Filter::default(),
+    );
 
     trainer.run(&schedule, &settings, &data);
 }
